f53c68a7386b00d0fa6bf9b91b285dbd757b383e
James O'Gorman <james@netinertia.co.uk>
2019-05-26 17:56:50 +0100

    apiserver: Implement gRPC server
    This restructures the apiserver a bit to allow running the HTTP and gRPC
    servers concurrently. The database connection pool is shared between
    both.

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/jamesog/example-http-api/api"
+	"github.com/jamesog/example-http-api/database"
 	"github.com/jamesog/example-http-api/database/postgres"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	listen := os.Getenv("LISTEN_ADDR")
+func httpServer(db database.Storage) {
+	listen := os.Getenv("HTTP_ADDR")
 	if listen == "" {
 		listen = ":8000"
 	}
 
-	db, err := postgres.NewDB("sslmode=disable user=postgres dbname=example")
-	if err != nil {
-		log.Fatal(err)
-	}
 	apisvc := api.NewService(db)
 
 	r := chi.NewRouter()
@@ -28,4 +27,32 @@ func main() {
 	r.Mount("/", apisvc.Routes())
 
 	log.Fatal(http.ListenAndServe(listen, r))
+}
+
+func grpcServer(db database.Storage) {
+	listen := os.Getenv("GRPC_ADDR")
+	if listen == "" {
+		listen = ":8001"
+	}
+
+	lis, err := net.Listen("tcp", listen)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apisvc := api.NewService(db)
+
+	grpcServer := grpc.NewServer()
+	api.RegisterExampleServiceServer(grpcServer, apisvc)
+	log.Fatal(grpcServer.Serve(lis))
+}
+
+func main() {
+	db, err := postgres.NewDB("sslmode=disable user=postgres dbname=example")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go httpServer(db)
+	grpcServer(db)
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.12
 
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/golang/protobuf v1.3.1
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.1.1
 	github.com/rs/zerolog v1.14.3
 	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
 	google.golang.org/appengine v1.6.0 // indirect
+	google.golang.org/grpc v1.21.0
 )
